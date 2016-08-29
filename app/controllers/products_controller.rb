f697d0ca468568f46f0f9fa78ba46b2e2d5dbfae
Jen <jvarelalijo@gmail.com>
2016-08-29 10:19:33 +0200

    5.2 Forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,18 @@ class ProductsController < ApplicationController
 
   # GET /products
   # GET /products.json
-  def index
-    @products = Product.all
-  end
+def index
+  if params[:q]
+    search_term = params[:q]
+    if (Rails.env == "production")
+      @products = Product.where("name ilike ?", "%#{search_term}%")
+    else
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    end
+    else
+      @products = Product.all
+    end
+end
 
   # GET /products/1
   # GET /products/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'static_pages/landing_page'
-
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
