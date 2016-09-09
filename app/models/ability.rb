794987d33f4b60153249c605099767c27cd644f0
Jen <jvarelalijo@gmail.com>
2016-09-09 12:49:26 +0200

    fixing comments bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,12 +29,14 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
     def initialize(user)
         user ||= User.new # guest user (not logged in)
-        can :manage, User, id: user.id
+        
         if user.admin?
             can :manage, :all
         else
-            can :read, :all
+            can :manage, User, id: user.id
+            cannot :destroy, Comment
         end
+   
     end
 
 end
