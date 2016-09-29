7eaa1b9b67e10f4162cd4992bb19961f1ba98bba
Jen <jvarelalijo@gmail.com>
2016-09-29 22:47:44 +0200

    payment + atom

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,37 +7,34 @@ class PaymentsController < ApplicationController
 		#declaring instance variable to refer to the current user by using Devise helper.
 		@user = current_user
 		token = params[:stripeToken]
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-  	charge = Stripe::Charge.create(
-	      :amount => @product.price.to_i*100, # amount in cents, again
-	      :currency => "eur",
-	      :source => token,
-	      :description => params[:stripeEmail]
-	      )
+		# Create the charge on Stripe's servers - this will charge the user's card
+		begin
+			charge = Stripe::Charge.create(
+			:amount => @product.price.to_i*100, # amount in cents, again
+			:currency => "eur",
+			:source => token,
+			:description => params[:stripeEmail]
+			)
 
-  	if charge.paid
-  		Order.create(
-  			:product_id => @product,
-  			:user_id => @user,
-  			:total => @product.price
-  			)
+			if charge.paid
+				Order.create(
+				:product_id => @product,
+				:user_id => @user,
+				:total => @product.price
+				)
 
-  		flash[:success] = "Payment successful"
-  		redirect_to "payments/payment_successful.html.erb"
-  		
-  	end
+				flash[:success] = "Payment successful"
+				redirect_to "payments/payment_successful.html.erb"
 
-  rescue Stripe::CardError => e
-	      # The card has been declined
-	      body = e.json_body
-	      err = body[:error]
-	      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-	    end
+			end
 
-    redirect_to product_path(@product) #redirects to product page
+		rescue Stripe::CardError => e
+			# The card has been declined
+			body = e.json_body
+			err = body[:error]
+			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+		end
 
-  end
+	end
 
 end
-
