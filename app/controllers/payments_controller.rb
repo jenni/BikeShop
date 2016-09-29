e78cb7ad63000159c5082d0e769555622bc93db3
Jen <jvarelalijo@gmail.com>
2016-09-29 22:35:37 +0200

    payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-  before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def create
 		#declaring instance variable to find product by reading hidden field's params in the stripe checkout button form.
@@ -8,34 +8,34 @@ class PaymentsController < ApplicationController
 		@user = current_user
 		token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
-	  begin
-	  	charge = Stripe::Charge.create(
+  begin
+  	charge = Stripe::Charge.create(
 	      :amount => @product.price.to_i*100, # amount in cents, again
 	      :currency => "eur",
 	      :source => token,
 	      :description => params[:stripeEmail]
 	      )
 
-	  	if charge.paid
-	  		Order.create(
-	  			:product_id => @product,
-	  			:user_id => @user,
-	  			:total => @product.price
-	  			)
+  	if charge.paid
+  		Order.create(
+  			:product_id => @product,
+  			:user_id => @user,
+  			:total => @product.price
+  			)
 
-	  		flash[:success] = "Payment successful"
-	  		redirect_to "payments/payment_successful.html.erb"
-	  	
-      end
+  		flash[:success] = "Payment successful"
+  		redirect_to "payments/payment_successful.html.erb"
+  		
+  	end
 
-	  rescue Stripe::CardError => e
+  rescue Stripe::CardError => e
 	      # The card has been declined
 	      body = e.json_body
 	      err = body[:error]
 	      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-    end
+	    end
 
-    redirect_to static_pages_thanks_payment_path #redirects to product page
+    redirect_to product_path(@product) #redirects to product page
 
   end
 
