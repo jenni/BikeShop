1d4678746b893ce2832bd8c6c99eedda9bcfbc6a
Jen <jvarelalijo@gmail.com>
2016-09-30 13:25:22 +0200

    6.9 JS on Rails

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,17 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  $('.img-zoom').elevateZoom({
+    zoomType: "lens",
+    lensShape: "round",
+    lensSize: 180,
+    scrollZoom: true,
+    lensFadeIn: 800,
+    lensFadeOut: 800
+  });
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+  readOnly: true,
+  score: function() {
+    return $(this).attr('data-score');
+  }
+});
 });
