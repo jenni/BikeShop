ecca3699dcff5d087e5a4ea0f26819af44de4baf
Jen <jvarelalijo@gmail.com>
2016-10-02 13:53:44 +0200

    6.11 Caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+  # enable caching in development mode: $ rails dev:cache
+  # code means if the file caching-dev.txt is found in the /tmp folder, caching will be switched on.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
